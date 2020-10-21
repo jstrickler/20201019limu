b6eedd37535fef307d0c283f38af7e3873687435
jstrickler <jstrickler@gmail.com>
2020-10-21 10:30:06 -0400

    10:30AM Wed; added slides

--- EXAMPLES/dict_comprehension.py
+++ EXAMPLES/dict_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import os
 
 animals = ['OWL', 'Badger', 'bushbaby', 'Tiger', 'Wombat', 'GORILLA', 'AARDVARK']
 
@@ -7,9 +7,31 @@ d = {a.lower(): len(a) for a in animals}  # <1>
 
 print(d, '\n')
 
+file_names = ['../DATA/alice.txt', '../DATA/mary.txt', '../DATA/parrot.txt']
+
+file_sizes = {os.path.basename(fn): os.path.getsize(fn) for fn in file_names}
+print("file_sizes:", file_sizes, '\n')
+
+# nested dictionary comprehensions!
+
 words = ['unicorn', 'stigmata', 'barley', 'bookkeeper']
 
+# a little convoluted, but fun:
 d = {w:{c:w.count(c) for c in sorted(w)} for w in words} # <2>
 
 for word, word_signature in d.items():
     print(word, word_signature)
+print()
+
+# equivalent to:
+all_counts = {}
+for word in words:
+    counts = {}
+    for c in sorted(word):
+        counts[c] = word.count(c)
+    all_counts[word] = counts
+
+for word, word_signature in d.items():
+    print(word, word_signature)
+
+# facetiously
--- EXAMPLES/set_comprehension.py
+++ EXAMPLES/set_comprehension.py
@@ -2,6 +2,15 @@
 
 import re
 
-with open("../DATA/mary.txt") as mary_in:
+with open("../DATA/alice.txt") as mary_in:
     s = {w.lower()  for ln in mary_in  for w in re.split(r'\W+', ln) if w} #<1>
-print(s)
+print(sorted(s))
+
+food = ['Spam', 'spaM', 'spam', 'spam', 'spam', 'spam', 'HAM',
+        'spam', 'spam', 'SPAM', 'spam', 'Spam', 'Ham', 'ham']
+
+unique_food = {w.lower() for w in food}
+print(unique_food)
+
+semi_unique_food = set(food)
+print(semi_unique_food)
